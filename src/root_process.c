616a601c0ffa4b363633003e41dd9e4f5f6ab15d
Robert Wang <robwang.us@gmail.com>
2023-10-15 14:05:21 -0500

    worked on main() in root_process.c

--- src/root_process.c
+++ src/root_process.c
@@ -14,6 +14,7 @@ void redirection(char **dup_list, int size, char* root_dir){
     // TODO(overview): redirect standard output to an output file in output_file_folder("output/final_submission/")
     // TODO(step1): determine the filename based on root_dir. e.g. if root_dir is "./root_directories/root1", the output file's name should be "root1.txt"
 
+
     //TODO(step2): redirect standard output to output file (output/final_submission/root*.txt)
 
     //TODO(step3): read the content each symbolic link in dup_list, write the path as well as the content of symbolic link to output file(as shown in expected)
@@ -41,20 +42,51 @@ int main(int argc, char* argv[]) {
     }
 
     //TODO(overview): fork the first non_leaf process associated with root directory("./root_directories/root*")
-
+    
     char* root_directory = argv[1];
     char all_filepath_hashvalue[4098]; //buffer for gathering all data transferred from child process
     memset(all_filepath_hashvalue, 0, sizeof(all_filepath_hashvalue));// clean the buffer
 
     //TODO(step1): construct pipe
+    int fd[2];
+
+    if(pipe(fd) == -1){
+        perror("pipe");
+        exit(1);
+    }
 
     //TODO(step2): fork() child process & read data from pipe to all_filepath_hashvalue
+    pid_t first_proc;
 
+    first_proc = fork();
+
+    if(first_proc < 0){
+        perror("fork");
+        exit(1);
+    }
+    else if(first_proc == 0){   // Child process
+        close(fd[0]); // Close read end
+
+        execl("./nonleaf_process", root_directory, "1", (char *)NULL);
+
+        close(fd[1]);
+    }
+    else{ // Parent process
+        close(fd[1]);  // Close write end
+
+        // Read in file hashes and aggregate the file hashes
+        read(fd[0], all_filepath_hashvalue, 4098);
+
+        close(fd[0]);
+    }
 
     //TODO(step3): malloc dup_list and retain list & use parse_hash() in utils.c to parse all_filepath_hashvalue
     // dup_list: list of paths of duplicate files. We need to delete the files and create symbolic links at the location
     // retain_list: list of paths of unique files. We will create symbolic links for those files
-
+    char* dup_list = malloc(sizeof(all_filepath_hashvalue));
+    char* retain_list = malloc(sizeof(all_filepath_hashvalue));
+    
+    int idx = parse_hash(all_filepath_hashvalue, &dup_list, &retain_list);
 
     //TODO(step4): implement the functions
     delete_duplicate_files(dup_list,size);
@@ -62,5 +94,6 @@ int main(int argc, char* argv[]) {
     redirection(dup_list, size, argv[1]);
 
     //TODO(step5): free any arrays that are allocated using malloc!!
-
+    free(dup_list);
+    free(retain_list);
 }
