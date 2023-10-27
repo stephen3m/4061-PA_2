fa87b9dd4e89c32e39bdf7d4a80a9fcef42d1f4b
Robert Wang <wan00379@umn.edu>
2023-10-26 19:09:51 -0500

    new utils.c file

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "../include/utils.h"
 
 // Extracts the filename from the full path
@@ -38,87 +39,89 @@ void remove_filepath_duplicate(char **dup_list, char **retain_list, int *size) {
     for (int i = 0; i < *size; i++) {
         for (int j = i + 1; j < *size; ) {
             if (strcmp(dup_list[i], dup_list[j]) == 0) {
-                // Duplicate found, free the memory of the duplicate string
                 free(dup_list[j]);
                 free(retain_list[j]);
-
-                // Shift all the elements after j to the left
                 for (int k = j; k < *size - 1; k++) {
                     dup_list[k] = dup_list[k + 1];
                     retain_list[k] = retain_list[k+1];
                 }
-
-                (*size)--; // Decrease the size of the array
+                // Set the last items to NULL after shifting
+                dup_list[*size - 1] = NULL;
+                retain_list[*size - 1] = NULL;
+                (*size)--;
             } else {
-                j++; // Move to the next string if current one is not a duplicate
+                j++;
             }
         }
     }
 }
 
-// Stephen note: ensures that both retain list and dup list will have same number of entries
 void sanitize_dup_retain(char **dup_list, char **retain_list, int size) {
     for (int m = 0; m < size; m++) {
         for (int n = 0; n < size; n++) {
             if (strcmp(retain_list[m], dup_list[n]) == 0) {
-                // Found a match, replace retain_list[m] with retain_list[n]
-                // But first, if retain_list[m] was dynamically allocated, free it
                 if (retain_list[m] != retain_list[n]) {
                     free(retain_list[m]);
-                    retain_list[m] = retain_list[n];
+                    retain_list[m] = dup_list[n];
                 }
-                break; // No need to continue inner loop for this m once a match is found
+                break;
             }
         }
     }
 }
 
-// Stephen note: parse a string containing file hashes and extract information to fill duplist and retainlist
-int parse_hash(char * file_hashes, char**dup_list, char** retain_list){
+int parse_hash(char * file_hashes, char** dup_list, char** retain_list) {
     char delim[] = "|";
     char array[32][128];
     int count = 0;
-    memset(&array[0], 0, sizeof(array));// clean the buffer
+
+    memset(array, 0, sizeof(array));
 
     char *ptr = strtok(file_hashes, delim);
-
-    while(ptr != NULL)
-    {
-        strncpy(array[count], ptr, strlen(ptr));
+    while(ptr != NULL && count < 32) {
+        strncpy(array[count], ptr, sizeof(array[count]) - 1);
         ptr = strtok(NULL, delim);
         count++;
     }
 
     int index = 0;
-    //even idx is filename and odd index is the hash
-    for(int i=1; i<count*2; i+=2){
-        for (int j = i + 2; j < count*2; j+=2){
-            if (  strcmp(array[i], "") != 0 && strcmp(array[i], array[j]) == 0) {
-                // record the filepath of duplicate file
-                dup_list[index] = malloc(sizeof(char)* 128);
-                retain_list[index] = malloc(sizeof(char)* 128);
-                char* file_index_str_1 = &array[j-1][strlen(array[j-1])-5];
-                char* file_index_str_2 = &array[i-1][strlen(array[i-1])-5];
-                int file_index_1 = atoi(&file_index_str_1[0]);
-                int file_index_2 = atoi(&file_index_str_2[0]);
-                if(file_index_1 > file_index_2) {
-                    strcpy(dup_list[index], array[j - 1]);
-                    strcpy(retain_list[index], array[i - 1]);
 
-                }else{
-                    strcpy(dup_list[index], array[i - 1]);
-                    strcpy(retain_list[index], array[j - 1]);
+    for(int i = 1; i < count; i += 2) {
+        int retain_index = i; // index of file to be retained
+
+        // Look for other files with the same hash
+        for (int j = i + 2; j < count; j += 2) {
+            if (strcmp(array[i], array[j]) == 0) {
+                
+                if (retain_index == i) { // if first match, add i-1 to retain_list
+                    retain_list[index] = malloc(sizeof(char) * 128);
+                    if (!retain_list[index]) {
+                        fprintf(stderr, "Memory allocation failed!\n");
+                        exit(1);
+                    }
+                    strcpy(retain_list[index], array[retain_index - 1]);
                 }
-
+                
+                // add j-1 to dup_list
+                dup_list[index] = malloc(sizeof(char) * 128);
+                if (!dup_list[index]) {
+                    fprintf(stderr, "Memory allocation failed!\n");
+                    exit(1);
+                }
+                strcpy(dup_list[index], array[j - 1]);
+                
                 index++;
+
+                // set the hash to empty string to avoid duplication
+                array[j][0] = '\0';
             }
         }
+
+        // if no match found, retain_index will still be i, so just move on
     }
-    // remove duplicate filepath in dup_list get from file_hashes
-    remove_filepath_duplicate(dup_list,retain_list,&index);
-    // files in retain_list might also appear in dup_list,
-    // since one file may have more than 2 duplicates
-    sanitize_dup_retain(dup_list,retain_list,index);
+
+    remove_filepath_duplicate(dup_list, retain_list, &index);
+    sanitize_dup_retain(dup_list, retain_list, index);
 
     return index;
 }
